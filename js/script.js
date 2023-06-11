24f727fca32baf956671931177ef9ac8ee2dc271
unknown <leonardolucasga@hotmail.com>
2023-06-11 00:12:16 -0300

    init commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+fetch(`https://rickandmortyapi.com/api/character`,{
+    method:'GET'
+})
+.then((response) => response.json())
+.then(function(json){
+    let container = document.querySelector('.container');
+    json.results.map(function(results){
+        container.innerHTML +=`
+        <div class="card">
+        <img src=` + results.image +`> 
+        <br>
+        <strong>` + results.name +`</strong>
+        <br>
+        <span>` + results.origin.name +`</span>
+        <br>
+        <hr>
+        
+        </div>`
+    })
+})
+ 
